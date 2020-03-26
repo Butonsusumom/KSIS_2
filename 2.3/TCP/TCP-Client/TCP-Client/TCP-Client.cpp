d979843f320bf1bf7ec8579089794c181660a87c
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2020-03-26 11:58:50 +0300

    Add files via upload

---
+++ 2.3/TCP/TCP-Client/TCP-Client/TCP-Client.cpp
@@ -0,0 +1,181 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define PORT 8080
+#define SERVER_ADDRESS "127.0.0.1"
+
+#define ON_SEND_NAME 0
+#define ON_SEND_SIZE 1
+#define ON_SEND_DATA 2
+
+#define BLOCK_SIZE 0xFFFF
+
+char* nameByPath(char* filePath) {
+    int i = strlen(filePath) - 1;
+    bool found = false;
+    while (i >= 0 && !found) {
+        if (filePath[i] == '/' || filePath[i] == '\\') {
+            found = true;
+            i++;
+        }
+        i--;
+    }
+    i++;
+    return &filePath[i];
+}
+
+int generateFile(char filePath[]) {
+    const int fileSize = ( 5 + rand() % 25 ) * 10000; 
+    std::ofstream out;         
+    out.open(filePath); 
+    if (out.is_open())
+    {
+
+        for (int i = 0; i < fileSize; i++) {
+            out << char(32 + rand()% 125);
+        }
+    
+        out.close();
+        return 0;
+    }
+    return 1; 
+}
+
+int main() {
+    srand(static_cast<unsigned int>(time(0)));
+	WSADATA wsaData;
+
+	//Инициализация библиотеки Winsock
+	if (WSAStartup(0x202, &wsaData)) {
+		printf("WSAStart error");
+		return 1;
+	}
+
+	//Создание сокета
+	SOCKET clientSocket;
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0); //TODO: IPPROTO_TCP
+	if (clientSocket < 0) {
+		printf("Error calling socket() function");
+		return 1;
+	}
+
+    //Установка соединения
+    sockaddr_in destAddress;
+    destAddress.sin_family = AF_INET;
+    destAddress.sin_port = htons(PORT);
+    HOSTENT* hostent;
+
+    // преобразование IP адреса из символьного в сетевой формат
+    if (inet_addr(SERVER_ADDRESS) != INADDR_NONE) {
+        destAddress.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
+    }
+    else {
+        // попытка получить IP адрес по доменному имени сервера
+        if (hostent = gethostbyname(SERVER_ADDRESS)) {
+            // hst->h_addr_list содержит не массив адресов,
+            // а массив указателей на адреса
+            ((unsigned long*)&destAddress.sin_addr)[0] = ((unsigned long**)hostent->h_addr_list)[0][0];
+        }
+        else {
+            printf("Invalid address %s\n", SERVER_ADDRESS);
+            closesocket(clientSocket);
+            WSACleanup();
+            return 1;
+        }
+    }
+
+    //Адрес сервера получен – пытаемся установить соединение 
+    if (connect(clientSocket, (sockaddr*)&destAddress, sizeof(destAddress))) {
+        printf("Connect error");
+        return 1;
+    }
+
+    printf("Connected successfully\n");
+
+    //Чтение и передача сообщений    
+    int answerSize;
+    int toSend = ON_SEND_NAME;
+    char filePath[MAX_PATH];
+
+    do {
+        std::cout << "Name of file: ";
+        std::cin >> filePath;
+    } while ( generateFile(filePath) );
+
+    char* fileName = nameByPath(filePath);
+
+    FILE* inFile;
+    char* fileBuffer;
+    long fileSize;
+
+    inFile = fopen(filePath, "rb");  
+    fseek(inFile, 0, SEEK_END);
+    fileSize = ftell(inFile);
+    rewind(inFile);    
+
+    char sizeInChar[10] = { 0 };
+    _itoa(fileSize, sizeInChar, 10);
+
+    fileBuffer = (char*) malloc(BLOCK_SIZE);
+    int blocks = 0;
+    int recivedSyze = 0;
+
+    char dontCare[1024] ;
+    int time_start = clock();
+    while ((answerSize = recv(clientSocket, dontCare, 1024, 0)) != SOCKET_ERROR) {
+
+        switch (toSend) {
+        case ON_SEND_NAME:
+            send(clientSocket, fileName, strlen(fileName) + 1, 0);
+            toSend++;
+            break;
+        case ON_SEND_SIZE:
+            send(clientSocket, sizeInChar, strlen(sizeInChar) + 1, 0);
+            toSend++;
+            break;
+        case ON_SEND_DATA:
+            if (blocks * BLOCK_SIZE <= fileSize) {
+                int realBytes = fread(fileBuffer, 1, BLOCK_SIZE, inFile);
+                //std::cout << "Real bytes" << realBytes << "\n";
+                send(clientSocket, fileBuffer, realBytes, 0);
+                blocks++;
+            }
+            else {
+                fclose(inFile);
+                toSend++;
+                std::cout << std::endl; 
+                std::cout << "Speed " << fileSize / ((clock() - time_start + 0.0000001) / 1000) << 
+                    std::cout.precision(4) << " Byte/sec" << std::endl;
+                recivedSyze = atoi(dontCare);
+                std::cout << std::endl;
+                std::cout << "Received " << recivedSyze << " package of " << blocks << " " << std::endl;
+                if (recivedSyze == blocks) std::cout << "All package recived." << std::endl;
+                else  std::cout << "Not all package recived." << std::endl;
+            }
+            break;
+        default: {
+            getchar();
+        }
+        }
+    }
+    
+
+    closesocket(clientSocket);
+    WSACleanup();
+   
+	return 0;
+}
---
+++ 2.3/TCP/TCP-Server/TCP-Server/TCP-Server.cpp
@@ -0,0 +1,146 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#pragma comment (lib, "Ws2_32.lib")
+#include <iostream>
+#include <fstream>
+
+#define PORT 8080
+
+#define ON_RECEIVE_NAME 0
+#define ON_RECEIVE_SIZE 1
+#define ON_RECEIVE_DATA 2
+
+#define BLOCK_SIZE 0xFFFF
+
+DWORD WINAPI ProcessClient(LPVOID client_socket);
+
+int main() {
+    WSADATA wsaData;
+    printf("TCP SERVER STARTED");
+
+    //Инициализация библиотеки сокетов
+    if (WSAStartup(0x0202, &wsaData)) {
+        printf("Error WSAStartup %d\n",  WSAGetLastError());
+        return 1;
+    }
+
+    // Создание сокета
+    SOCKET serverSocket;
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error socket %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    //Связывание сокета с локальным адресом
+    sockaddr_in local_addr;
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = htons(PORT);
+    local_addr.sin_addr.s_addr = 0;
+
+    if (bind(serverSocket, (sockaddr*) &local_addr, sizeof(local_addr))) {
+        printf("Error bind %d\n", WSAGetLastError());
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    // Ожидание подключений
+    // размер очереди – 0x100
+    if (listen(serverSocket, 0x100)) {
+        printf("Error listen %d\n", WSAGetLastError());
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    //Извлекаем сообщение из очереди
+    SOCKET client_socket;    // сокет для клиента
+    sockaddr_in client_addr; // адрес клиента (заполняется системой)
+    // функции accept необходимо передать размер структуры
+    int client_addr_size = sizeof(client_addr);
+
+    // цикл извлечения запросов на подключение из очереди
+    while ((client_socket = accept(serverSocket, (sockaddr*) &client_addr, &client_addr_size))) {
+        // пытаемся получить имя хоста
+        HOSTENT* hostName;
+        hostName = gethostbyaddr((char*) &client_addr.sin_addr.s_addr, 4, AF_INET);
+
+        // вывод сведений о клиенте
+        printf("+%s [%s] new connect!\n", (hostName) ? hostName->h_name : "", inet_ntoa(client_addr.sin_addr));
+            
+        DWORD thID;
+        CreateThread(NULL, NULL, ProcessClient, &client_socket, NULL, &thID);
+    }
+    return 0;
+}
+
+DWORD WINAPI ProcessClient(LPVOID clientSocket) {
+    SOCKET socket;
+    socket = ((SOCKET*)clientSocket)[0];
+    
+    // отправляем клиенту приветствие 
+    send(socket, "0", 1, 0);
+
+    int bytesReceived = 0;
+
+    int toReceive = ON_RECEIVE_NAME;
+    char fileName[MAX_PATH] = { 0 };
+    int fileSize;
+    char buffer[10] = { 0 };
+
+    bytesReceived = recv(socket, fileName, MAX_PATH, 0);
+    FILE* outFile;
+    outFile = fopen(fileName, "wb");
+    char* fileBuffer = new char[BLOCK_SIZE];
+    int bytesCount = 0, count = 0;
+    toReceive++;
+    while (bytesReceived && bytesReceived != SOCKET_ERROR) {
+      char buffer[200]; //time
+      _itoa(count, buffer, 10);
+      send(socket, buffer, strlen(buffer), 0);
+      //  send(socket, "0", 3, 0);
+
+        switch (toReceive) {
+            case ON_RECEIVE_NAME:
+                bytesReceived = recv(socket, fileName, MAX_PATH, 0);
+                outFile = fopen(fileName, "wb");
+                toReceive++;
+                break;
+            case ON_RECEIVE_SIZE:
+                bytesReceived = recv(socket, buffer, 10, 0);
+                fileSize = _atoi64(buffer);
+                std::cout << "File Size: " << fileSize << "\n";
+                toReceive++;
+                break;
+            case ON_RECEIVE_DATA:
+                if (bytesCount < fileSize) {
+                    bytesReceived = recv(socket, fileBuffer, BLOCK_SIZE, 0);
+                    fwrite(fileBuffer, 1, bytesReceived, outFile);
+                    bytesCount += bytesReceived;
+                    ++count;
+
+                }
+                else {
+                    fclose(outFile);
+                    toReceive++;
+
+                }
+                break;
+            default:
+                getchar();
+        }
+    }
+    printf("Disconnected :(");
+    closesocket(socket);
+    return 0;
+}
